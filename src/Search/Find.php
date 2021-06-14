04d9b4859dc6bb3b7e087ca379937ae101e35f90
André Baldeweg <kontakt@andrebaldeweg.de>
2021-06-14 18:37:08 +0200

    fix ordering

--- src/Search/Find.php
+++ src/Search/Find.php
@@ -46,7 +46,7 @@ class Find implements FindInterface
         $qb->leftJoin('b.author', 'a');
         $qb->leftJoin('b.tags', 't');
         $qb->leftJoin('b.genre', 'g');
-        $qb->distinct();
+        // $qb->distinct();
 
         if (isset($options['term']) || isset($options['filter'])) {
             $qb->where(
